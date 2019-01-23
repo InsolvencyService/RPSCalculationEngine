bea818ad9d62b5f403f5faf606b0183dde34ebc9
Stuart Reeve <stuart.reeve.dev@gmail.com>
2019-01-23 13:05:45 +0000

    RCA-295 Redundancy Payment - Negative Payments

--- Insolvency.CalculationsEngine.Redundancy.BL.UnitTests/ServicesTests/RedundancyPaymentCalculationServiceTests.cs
+++ Insolvency.CalculationsEngine.Redundancy.BL.UnitTests/ServicesTests/RedundancyPaymentCalculationServiceTests.cs
@@ -28,50 +28,26 @@ namespace Insolvency.CalculationsEngine.Redundancy.BL.UnitTests.ServicesTests
         [Trait("Category", "UnitTest")]
         [ClassData(typeof(RedundancyPaymentTestDataHelper.RedundancyPaymentRequestData))]
         public async Task PerformRedundancyPaymentCalculationForAllAgeGroups(
-            RedundancyPaymentCalculationRequestModel request1, RedundancyPaymentResponseDto response1,
-            RedundancyPaymentCalculationRequestModel request2, RedundancyPaymentResponseDto response2,
-            RedundancyPaymentCalculationRequestModel request3, RedundancyPaymentResponseDto response3,
-            RedundancyPaymentCalculationRequestModel request4, RedundancyPaymentResponseDto response4,
-            RedundancyPaymentCalculationRequestModel request5, RedundancyPaymentResponseDto response5)
+            RedundancyPaymentCalculationRequestModel request, RedundancyPaymentResponseDto expectedResult)
         {
-            var inputData = new List<RedundancyPaymentCalculationRequestModel>()
-            {
-                request1, request2, request3, request4, request5
-            };
-           
-            var expectedResult = new List<RedundancyPaymentResponseDto>()
-            {
-                response1, response2, response3, response4, response5
-            };
-            var outputResult = new List<RedundancyPaymentResponseDto>();
 
             //Act
-            foreach (var data in inputData)
-            {
-                var res =
-                    await _redundancyPaymentCalculationService.PerformRedundancyPayCalculationAsync(data, _options);
-                outputResult.Add(res);
-            }
+            var actualResult = await _redundancyPaymentCalculationService.PerformRedundancyPayCalculationAsync(request, _options);
+
             //Assert
-            int i = 0;
-            foreach (var actualResult in outputResult)
-            {
-                var expectedCalculationResult = expectedResult[i];
-                Assert.IsType<RedundancyPaymentResponseDto>(actualResult);
-                actualResult.AdjEmploymentStartDate.Should().Be(expectedCalculationResult.AdjEmploymentStartDate);
-                actualResult.NoticeDateForRedundancyPay.Should().Be(expectedCalculationResult.NoticeDateForRedundancyPay);
-                actualResult.NoticeEntitlementWeeks.Should().Be(expectedCalculationResult.NoticeEntitlementWeeks);
-                actualResult.RedundancyPayWeeks.Should().Be(expectedCalculationResult.RedundancyPayWeeks);
-                actualResult.YearsOfServiceUpto21.Should().Be(expectedCalculationResult.YearsOfServiceUpto21);
-                actualResult.YearsOfServiceFrom22To41.Should().Be(expectedCalculationResult.YearsOfServiceFrom22To41);
-                actualResult.YearsServiceOver41.Should().Be(expectedCalculationResult.YearsServiceOver41);
-                actualResult.GrossEntitlement.Should().Be(expectedCalculationResult.GrossEntitlement);
-                actualResult.EmployerPartPayment.Should().Be(expectedCalculationResult.EmployerPartPayment);
-                actualResult.NetEntitlement.Should().Be(expectedCalculationResult.NetEntitlement);
-                actualResult.PreferentialClaim.Should().Be(0m);
-                actualResult.NonPreferentialClaim.Should().Be(expectedCalculationResult.GrossEntitlement);
-                i++;
-            }
+            Assert.IsType<RedundancyPaymentResponseDto>(actualResult);
+            actualResult.AdjEmploymentStartDate.Should().Be(expectedResult.AdjEmploymentStartDate);
+            actualResult.NoticeDateForRedundancyPay.Should().Be(expectedResult.NoticeDateForRedundancyPay);
+            actualResult.NoticeEntitlementWeeks.Should().Be(expectedResult.NoticeEntitlementWeeks);
+            actualResult.RedundancyPayWeeks.Should().Be(expectedResult.RedundancyPayWeeks);
+            actualResult.YearsOfServiceUpto21.Should().Be(expectedResult.YearsOfServiceUpto21);
+            actualResult.YearsOfServiceFrom22To41.Should().Be(expectedResult.YearsOfServiceFrom22To41);
+            actualResult.YearsServiceOver41.Should().Be(expectedResult.YearsServiceOver41);
+            actualResult.GrossEntitlement.Should().Be(expectedResult.GrossEntitlement);
+            actualResult.EmployerPartPayment.Should().Be(expectedResult.EmployerPartPayment);
+            actualResult.NetEntitlement.Should().Be(expectedResult.NetEntitlement);
+            actualResult.PreferentialClaim.Should().Be(0m);
+            actualResult.NonPreferentialClaim.Should().Be(expectedResult.GrossEntitlement);
         }
     }
 }
