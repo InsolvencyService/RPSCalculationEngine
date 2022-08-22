ad12ce772f0ff4281039426e521675103ca3c22e
Glen Stone <glen.stone@insolvency.gov.uk>
2022-08-22 10:52:40 +0100

    APP-3156 Updated FluentValidation and fixed unit tests

--- Insolvency.CalculationsEngine.Redundancy.API/Controllers/RedundancyPaymentController.cs
+++ Insolvency.CalculationsEngine.Redundancy.API/Controllers/RedundancyPaymentController.cs
@@ -64,14 +64,16 @@ namespace Insolvency.CalculationsEngine.Redundancy.API.Controllers
                 }
 
                 var validator = new RedundancyPaymentCalculationRequestValidator();
-                if (!ModelState.IsValid || !validator.Validate(data).IsValid)
+                var validatorCheck = await validator.ValidateAsync(data);
+                var validatorErrors = validatorCheck.Errors.GetErrorsAsString();
+                if (!ModelState.IsValid || !validatorCheck.IsValid)
                 {
                     _logger.LogError(400, $"Request model not valid " +
                                           $"{ModelState}\n Request Data {JsonConvert.SerializeObject(data)} \n Errors : " +
-                                          $"{validator.Validate(data).Errors.GetErrorsAsString()} ");
+                                          $"{validatorErrors} ");
                     return BadRequest(ModelState);
                 }
-            
+
                 var result = await _rpCalculationsService.PerformRedundancyPayCalculationAsync(data, _options);
                 _logger.LogInformation((int)HttpStatusCode.OK,
                        $"Calculation performed successfully for the request data provided \n Request Data: {JsonConvert.SerializeObject(data)}");
